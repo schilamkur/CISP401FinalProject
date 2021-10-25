7bfa73874fa523b48a88198a4ceb3c587c28e214
schilamkur <91044775+schilamkur@users.noreply.github.com>
2021-10-24 21:54:44 -0700

    Add files via upload

---
+++ CISP401FinalProject/CISP401FinalProject/src/com/company/controllers/Main.java
@@ -0,0 +1,74 @@
+package com.company.controllers;
+
+import com.company.models.CalculateGPA;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static String [][] courseGrade = new String[100][2]; //0 is course name, 1 is course grade
+    // convert the above to class which contains 2 variables course and grade
+    private static int total = 0;
+    private static float gpa;
+
+    public static void main(String[] args) {
+
+        // Convert the reading of the input into classes and methods
+        // Enter Student grades associated with course taken
+        System.out.println("Enter Number of courses: ");
+        Scanner courseInput = new Scanner(System.in);
+        String inputString = courseInput.nextLine();
+        int numCourses = Integer.parseInt(inputString);
+
+        for(int i=0; i <numCourses; i++) {
+            System.out.println("Enter Course Name: ");
+            inputString = courseInput.nextLine();
+            courseGrade[i][0] = inputString;
+
+            System.out.println("Enter Course Grade: ");
+            inputString = courseInput.nextLine();
+            courseGrade[i][1] = inputString;
+
+            System.out.println("Course: " + courseGrade[i][0] + "  Grade: " +courseGrade[i][1]);
+
+        }
+
+
+        // Convert the calculation of the GPA into class and methods
+        // Calculating GPA
+        System.out.println("Before GPA class");
+        CalculateGPA calculateGPA = new CalculateGPA(courseGrade, numCourses);
+        System.out.println("After GPA");
+        double studentGPA = calculateGPA.getTotalGPA();
+        System.out.println("StudentGPA");
+
+  /* converted to class
+    for (int j = 0; j < numCourses; j++){
+        if (courseGrade[j][1].equals("A")){
+            total = total + 4;
+        }
+        else if (courseGrade[j][1].equals("B")){
+            total = total + 3;
+        }
+        else if (courseGrade[j][1].equals("C")){
+            total = total + 2;
+        }
+        else if (courseGrade[j][1].equals("D")) {
+            total = total + 1;
+        }
+        else {
+            total = total + 0;
+        }
+    }
+
+    gpa = total / numCourses;
+*/
+    System.out.println("You're GPA is: " + studentGPA);
+
+        //College matching - logic to be implemented
+        // Based on the GPA, match the possible college matches
+
+        // Read from File
+
+    }
+}
